97a74211855a7f3540981dff0ebb32f147e36b3c
Lam Nguyen <nguyenthanhlam091@gmail.com>
2024-03-19 09:57:56 +1100

    upload weather app

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Loading from '../assets/Loading.jpeg'
 import MusicBand from '../assets/MusicBand.png'
+import WeatherApp from '../assets/Weather_App.png'
 
 const Work = () => {
   return (
@@ -12,7 +13,7 @@ const Work = () => {
             </div>
 
             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-              <div style={{backgroundImage: `url(${MusicBand})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+              <div style={{backgroundImage: `url(${MusicBand})`}} className=' shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div '>
                
                 {/* Hover effect */}
                 <div className='opacity-0 group-hover:opacity-100'>
@@ -30,18 +31,18 @@ const Work = () => {
                 </div>
               </div>
 
-              <div style={{backgroundImage: `url(${Loading})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+              <div style={{backgroundImage: `url(${WeatherApp})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                
                 {/* Hover effect */}
                 <div className='opacity-0 group-hover:opacity-100'>
                   <span className='text-2xl font-bold text-white tracking-wider'>
-                      React js Application 
+                      Weather App 
                   </span>
                   <div className='pt-8 text-center'>
-                    <a href="/">
+                    <a href="https://liam263.github.io/Weather_App/" target='_blank'>
                       <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'> Demo</button>
                     </a>
-                    <a href="/">
+                    <a href="https://github.com/Liam263/Weather_App" target='_blank'>
                       <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                     </a>
                   </div>
