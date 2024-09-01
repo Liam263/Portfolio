c1a1ac9a75d42a48ede14757df7d15c9caf35bf6
Liam263 <nguyenthanhlam091@gmail.com>
2024-09-01 14:59:02 +1000

    update title

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,8 +10,7 @@ const Home = () => {
         <p className="text-4xl font-bold text-blue-500">Hi, my name is </p>
         <h1 className="text-4xl sm:text-7xl font-bold">Lam Nguyen</h1>
         <h2 className="text-4xl sm:text-5xl font-bold text-[#8892b0] max-w-[800px]">
-          A final-year student pursuing a Bachelor of Computer Science at the
-          University of Wollongong.
+          Full Stack Developer
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[800px]">
           Dedicated and motivated full-stack developer with expertise in
